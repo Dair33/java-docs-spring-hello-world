fc955060641cc5ce48ef2dfcfacc97c801a0b8f9
Dair33 <101737397+Dair33@users.noreply.github.com>
2023-07-27 15:54:29 +0100

    JSON Test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -6,8 +6,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
+	  private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+	@PostMapping("/name")
+public String createUser(@RequestBody String name) {
+    // code to create a user
+	    logger.info("Creating user with name: {}", name);
+		return name;
+
+}
 
 }
