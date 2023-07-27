25a7f4cfdfa97ccfbfaa51606430d1ffc8ee701f
Dair33 <101737397+Dair33@users.noreply.github.com>
2023-07-27 16:24:22 +0100

    Log 4

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,12 +11,12 @@ public class DemoApplication {
  private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		logger.info("This is a test log to make sure this works")
+		logger.info("This is a test log to make sure this works");
 	}
 
 	@RequestMapping("/api")
 	String sayHello() {
-			logger.info("This is a test log to make sure this works")
+			logger.info("This is a test log to make sure this works");
 		return "Hello World this is an update number 3 hopefully with working react ADADASDASDASD";
 	
 	}
