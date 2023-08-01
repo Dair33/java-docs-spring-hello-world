72fcf09d883a387ddba8b6ca8a23f9934bf4a3ab
John Doe <alasdairccball@gmail.com>
2023-08-01 13:03:17 +0100

    s

---
+++ src/main/java/com/example/demo/HealthCheckController.java
@@ -0,0 +1,26 @@
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@RestController
+public class HealthCheckController {
+
+    private final DataSource dataSource;
+
+    public HealthCheckController(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        try (Connection connection = dataSource.getConnection()) {
+            return ResponseEntity.ok("Database connection is OK");
+        } catch (SQLException e) {
+            return ResponseEntity.status(500).body("Database connection failed");
+        }
+    }
+}
