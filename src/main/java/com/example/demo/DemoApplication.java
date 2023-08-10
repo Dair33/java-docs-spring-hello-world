9641fd8b98199cdf8dda725d3ff82f48ebea7d42
Dair33 <101737397+Dair33@users.noreply.github.com>
2023-08-10 15:33:23 +0100

    v

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,28 +3,37 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+import java.util.Map;
+
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-
+	 private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 	public static void main(String[] args) {
+	
+
 		SpringApplication.run(DemoApplication.class, args);
+
 	}
 
-	@RequestMapping("/hello")
+	@RequestMapping("/")
 	String sayHello() {
-		return "Hello World!";
+        logger.info("This is a test log tomake sure this works");
+		return "Hello World this is an updatenumber 3 hopefully with working react ADADASDASDASD";
+	
 	}
-	  @PostMapping("/name")
-    public String receiveData(@RequestBody MyData data) {
-        // Do something with the data
-        System.out.println("Received name: " + data.body);
-        
-        return "Data received successfully";
+	@PostMapping("/name")
+    public void postName(@RequestBody Map<String, String> body) {
+        String name = body.get("name");
+        logger.info("Received me {}", name);
     }
 }
