131593b2e8c3264216f04639ec7d618d9ec4421e
Dair33 <101737397+Dair33@users.noreply.github.com>
2023-08-10 15:31:01 +0100

    v

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
@@ -17,4 +20,11 @@ public class DemoApplication {
 	String sayHello() {
 		return "Hello World!";
 	}
+	  @PostMapping("/name")
+    public String receiveData(@RequestBody MyData data) {
+        // Do something with the data
+        System.out.println("Received name: " + data.body);
+        
+        return "Data received successfully";
+    }
 }
