c9458ae838f4398dfe8350e1039da0bfbb6b44d4
Dair33 <101737397+Dair33@users.noreply.github.com>
2023-08-11 17:51:15 +0100

    M

--- src/main/java/com.example.mdspringboot/MdbSpringBootApplication.java
+++ src/main/java/com.example.mdspringboot/MdbSpringBootApplication.java
@@ -6,43 +6,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-
 
 import java.util.Map;
 
 
-@SpringBootApplication
-@RestController
-public class DemoApplication {
-	 private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
-	public static void main(String[] args) {
-	
-
-		SpringApplication.run(DemoApplication.class, args);
-
-	}
 
 @EnableMongoRepositories
-public class MdbSpringBootApplication implements CommandLineRunner{
-    
+@RestController
+public class MdbSpringBootApplication implements CommandLineRunner {
+    private static final Logger logger = LoggerFactory.getLogger(MdbSpringBootApplication.class);
+
     @Autowired
     ItemRepository groceryItemRepo;
     
     public static void main(String[] args) {
         SpringApplication.run(MdbSpringBootApplication.class, args);
     }
-}
 
-	@RequestMapping("/hello")
-	String sayHello() {
-        logger.info("This is a test log tomake sure this works");
-		return "Hello World this is an updatenumber 3 hopefully with working react ADADASDASDASD";
-	
-	}
-	@PostMapping("/name")
+    @Override
+    public void run(String... args) {
+        // Logic to be run on application start, for example:
+        // groceryItemRepo.save(new GroceryItem("1", "Apple", 10, "Fruits"));
+    }
+    @RequestMapping("/hello")
+    String sayHello() {
+        logger.info("This is a test log to make sure this works");
+        return "Hello World this is an update number 3 hopefully with working react ADADASDASDASD";
+    }
+
+    @PostMapping("/name")
     public void postName(@RequestBody Map<String, String> body) {
         String name = body.get("name");
         logger.info("Received me {}", name);
