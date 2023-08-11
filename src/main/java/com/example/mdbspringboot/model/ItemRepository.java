b3f0bba93f07511bb7ca7c88c1faaf4df44a2724
John Doe <alasdairccball@gmail.com>
2023-08-11 18:56:09 +0100

    M

---
+++ src/main/java/com/example/mdbspringboot/model/ItemRepository.java
@@ -0,0 +1,21 @@
+package com.example.mdbspringboot.model;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import scala.collection.immutable.List;
+
+public interface ItemRepository extends MongoRepository<GroceryItem, String> {
+    
+    @Query("{name:'?0'}")
+    GroceryItem findItemByName(String name);
+    
+    @Query(value="{category:'?0'}", fields="{'name' : 1, 'quantity' : 1}")
+    List<GroceryItem> findAll(String category);
+    
+    public long count();
+
+}
