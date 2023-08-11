d36323ecc5291dd30562234a7d45293db6f6833c
Dair33 <101737397+Dair33@users.noreply.github.com>
2023-08-11 18:38:54 +0100

    M

--- src/main/java/com.example.mdspringboot.repository/ItemRepository.java
+++ src/main/java/com.example.mdspringboot.repository/ItemRepository.java
@@ -3,21 +3,14 @@ package com.example.mdbspringboot.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document("groceryitems")
-public class GroceryItem {
+public interface ItemRepository extends MongoRepository<GroceryItem, String> {
+    
+    @Query("{name:'?0'}")
+    GroceryItem findItemByName(String name);
+    
+    @Query(value="{category:'?0'}", fields="{'name' : 1, 'quantity' : 1}")
+    List<GroceryItem> findAll(String category);
+    
+    public long count();
 
-        @Id
-        private String id;
-
-        private String name;
-        private int quantity;
-        private String category;
-        
-        public GroceryItem(String id, String name, int quantity, String category) {
-            super();
-            this.id = id;
-            this.name = name;
-            this.quantity = quantity;
-            this.category = category;
-        }
 }
